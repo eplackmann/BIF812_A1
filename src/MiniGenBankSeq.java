abc29fd9f1d53d3a463b26e223fcbf1ff54046ac
Eric Plackmann <manager@bl8vbox.unassigned-domain>
2016-02-16 19:22:59 -0500

    Updated MiniGenBankSeq class, updated constructors/get/set, added comments. Updated UseMiniGenBankSeq class, updated method calling, added comments.

--- src/MiniGenBankSeq.java
+++ src/MiniGenBankSeq.java
@@ -1,62 +1,67 @@
 
 public class MiniGenBankSeq {
+	
+	// Initial declaration of arguments in class
 	private String locus;
 	private String accession;
 	private String definition;
 	private String source;
 	
-	public void setLocus(String newValue){
+	public void setLocus(String newValue){ // sets Locus argument
 		locus = newValue;
 	}
 	
-	public String getLocus(){
+	public String getLocus(){ // prints warning message if void, returns Locus
 		if (locus==null) {
 			printError("locus");
 		}
 		return locus;
 	}
 	
-	public void setAccess(String newValue){
+	public void setAccess(String newValue){ // sets Accession argument
 		accession = newValue;
 	}
 	
-	public String getAccess(){
+	public String getAccess(){ // prints warning message if void, returns Accession
 		if (accession==null) {
 			printError("accession");
 		}
 		return accession;
 	}
 	
-	public void setDef(String newValue){
+	public void setDef(String newValue){ // sets Definition argument
 		definition = newValue;
 	}
 	
 	public String getDef(){
-		if (definition==null) {
+		if (definition==null) { // prints warning if void, returns Definition
 			printError("definition");
 		}
 		return definition;
 	}
 	
-	public void setSource(String newValue){
+	public void setSource(String newValue){ // sets Source argument
 		source = newValue;
 	}
 	
-	public String getSource(){
+	public String getSource(){ // prints warning if void, returns Source
 		if (source==null) {
 			printError("source");
 		}
 		return source;
 	}
 	
-	public void printError(String x){
-		System.out.println("WARNING, " + x + " not initialized.");
+	// Prints out warning string depending on method being called
+	public void printError(String x){ 
+		System.out.printf("WARNING, %s not initialized.\n", x);
 	}
 	
+	// No argument constructor
 	public MiniGenBankSeq(){
 		
 	}
 	
+	// Four argument constructor
 	public MiniGenBankSeq(String newLocus, String newAccess, String newDef, String newSource){
 		this.locus = newLocus;
 		this.accession = newAccess;
@@ -64,6 +69,7 @@ public class MiniGenBankSeq {
 		this.source = newSource;
 	}
 	
+	// Two argument constructor
 	public MiniGenBankSeq(String newLocus, String newAccess){
 		this(newLocus, newAccess, null, null);
 	}	
--- src/UseMiniGenBankSeq.java
+++ src/UseMiniGenBankSeq.java
@@ -1,17 +1,49 @@
-
+/**
+ * Main method for running the MiniGenBankSeq class.
+ * Provides varying set argument levels to run different constructors,
+ * followed by full instantiation of objects for comparison of output display (ie. warnings)
+ * GenBank homo sapien data obtained from NCBI website http://www.ncbi.nlm.nih.gov/genome/?term=homo+sapien
+ * @author Eric Plackmann
+ *
+ */
 public class UseMiniGenBankSeq {
 
 	public static void main(String[] args) {
-		//prints out the different class objects
-		System.out.println("Object with no arguments initialized:");
-		MiniGenBankSeq noargs = new MiniGenBankSeq();
-		System.out.println(noargs.toString());
-		System.out.println("Object with all arguments initialized:");
-		MiniGenBankSeq fourargs = new MiniGenBankSeq("one", "two", "three", "four");
-		System.out.println(fourargs.toString());
-		System.out.println("Object with two arguments initialized:");
-		MiniGenBankSeq twoargs = new MiniGenBankSeq("one", "two");
+		
+		// Initialize and display uninitialized object
+		System.out.println("Displaying 'uninitialized' object with no-arguments constructor:");
+		MiniGenBankSeq uninitialized = new MiniGenBankSeq();
+		System.out.println(uninitialized.toString());
+		System.out.println("");
+		
+		// Setting arguments of uninitialized object
+		System.out.println("Displaying 'uninitialized' object after setting arguments:");
+		uninitialized.setLocus("NC_000001");
+		uninitialized.setAccess("NC_000001 GPC_000001293");
+		uninitialized.setDef("Homo sapiens chromosome 1, GRCh38.p2 Primary Assembly.");
+		uninitialized.setSource("Homo sapiens (human)");
+		System.out.println(uninitialized.toString());
+		System.out.println("");
+		
+		// Initializing object with two argument constructor
+		System.out.println("Displaying 'twoarg' object using two argument constructor:");
+		MiniGenBankSeq twoargs = new MiniGenBankSeq("NC_000003", "NC_000003 GPC_000001295");
 		System.out.println(twoargs.toString());
+		System.out.println("");
+		
+		//Setting remaining arguments in twoarg object
+		System.out.println("Displaying 'twoarg' object after setting arguments:");
+		uninitialized.setDef("Homo sapiens chromosome 3, GRCh38.p2 Primary Assembly.");
+		uninitialized.setSource("Homo sapiens (human)");
+		System.out.println(uninitialized.toString());
+		System.out.println("");
+		
+		// Initializing object with all arguments set
+		System.out.println("Displaying 'fourarg' object using four argument constructor:");
+		MiniGenBankSeq fourargs = new MiniGenBankSeq("NC_000002", "NC_000002 GPC_000001294", "Homo sapiens chromosome 2, GRCh38.p2 Primary Assembly", "Homo sapiens (human)");
+		System.out.println(fourargs.toString());
+		System.out.println("");
+		
 	}
 
 }
